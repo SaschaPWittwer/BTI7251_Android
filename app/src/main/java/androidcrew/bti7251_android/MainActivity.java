ef47f80f6978f51288a972f82c185b9523f90e5d
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2018-06-22 15:59:10 +0200

    Start implementing intents

--- app/src/main/java/androidcrew/bti7251_android/MainActivity.java
+++ app/src/main/java/androidcrew/bti7251_android/MainActivity.java
@@ -1,5 +1,7 @@
 package androidcrew.bti7251_android;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onAsyncTaskButtonClick(View btn){
         new TestTask(this).execute(5);
+    }
+
+    public void onImageIntentButtonClick(View btn) {
+        //Intent imageIntent = new Intent()
     }
 }
