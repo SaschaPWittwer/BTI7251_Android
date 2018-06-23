fddea68c3a32d9ddeee7f6ea45bf68d931d0bd89
baiuj1 <joris.baiutti@students.bfh.ch>
2018-06-23 11:51:22 +0200

    Start Intent with callback

---
+++ app/src/main/java/androidcrew/bti7251_android/IntentWithReturnValue.java
@@ -0,0 +1,28 @@
+package androidcrew.bti7251_android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class IntentWithReturnValue extends AppCompatActivity {
+    final static String returKey = "RETURN_VALUE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_with_return_value);
+
+        EditText editText = findViewById(R.id.texttoreturn);
+        Button button = findViewById(R.id.buttonsendtexttomain);
+
+        button.setOnClickListener(l -> {
+            Intent output = new Intent();
+            output.putExtra(returKey, editText.getText().toString());
+            setResult(RESULT_OK, output);
+            finish();
+        });
+
+    }
+}
--- app/src/main/java/androidcrew/bti7251_android/MainActivity.java
+++ app/src/main/java/androidcrew/bti7251_android/MainActivity.java
@@ -8,18 +8,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int ACTIVITY_REQUEST_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.buttonFragment);
-        button.setOnClickListener(l -> {
+        Button fragmentbutton = (Button)findViewById(R.id.buttonFragment);
+        fragmentbutton.setOnClickListener(l -> {
             Intent intent = new Intent(this, FragmentHost.class);
             startActivity(intent);
+        });
+
+        Button callBackButton = (Button)findViewById(R.id.buttonactivitycallback);
+        callBackButton.setOnClickListener(l -> {
+            Intent intent = new Intent(this, IntentWithReturnValue.class);
+            startActivityForResult(intent, ACTIVITY_REQUEST_ID);
         });
 
     }
@@ -30,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void onImageIntentButtonClick(View btn) {
         //Intent imageIntent = new Intent()
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == ACTIVITY_REQUEST_ID) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                String returnValue = data.getStringExtra(IntentWithReturnValue.returKey);
+                TextView textView = findViewById(R.id.textviewcallback);
+                textView.setText(returnValue);
+
+            }
+        }
     }
 
 
